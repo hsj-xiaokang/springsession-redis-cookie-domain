f0fdade94d558b38484c3cafec48b669406374fb
2356899074@qq.com <2356899074@qq.com>
2017-09-19 16:52:57 +0800

    -------------------spring-session-comfig

---
+++ HttpSessionConfig - 或者这样.java
@@ -0,0 +1,32 @@
+package com.gstar.acp.configurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionStrategy;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * 配置spring sesssion cookie根域名为"/"多模块session共享
+* @author 作者heshengjin E-mail:2356899074@qq.com
+* @version 创建时间：2017年9月19日 上午11:21:42
+* 类说明
+*/
+@Configuration  
+@EnableRedisHttpSession   
+public class HttpSessionConfig {  
+     @Bean  
+     public CookieHttpSessionStrategy cookieHttpSessionStrategy() {  
+         CookieHttpSessionStrategy strategy = new CookieHttpSessionStrategy();  
+         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer(); 
+         cookieSerializer.setCookiePath("/");
+         cookieSerializer.setUseSecureCookie(true);
+         cookieSerializer.setUseHttpOnlyCookie(true);
+         cookieSerializer.setUseBase64Encoding(true);
+         cookieSerializer.setDomainName("www.springsession.com");
+         cookieSerializer.setCookieMaxAge(1000*60*8);
+         strategy.setCookieSerializer(cookieSerializer);  
+         return strategy;  
+     }  
+}  
